500781e02a66da240d042e6d7ec532d2d21c53b8
tsw <3033526150@qq.com>
2026-08-19 10:25:53 +0800

    基础项目提交

---
+++ src/test/java/cn/djct/stockdemo/StockDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.djct.stockdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
